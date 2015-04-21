c94395d0a2337832a12fc1c0dcbaf44cb8ae8181
ThangTheMan <thang@fromttm.com>
2015-04-21 15:30:42 -0700

    update

--- js/thang.js
+++ js/thang.js
@@ -3,3 +3,35 @@
  */
 
 
+$(document).ready(function() {
+    //Button to pop-out form and replace variable.
+    //$('#edit-1').click(function () {
+    //    $('#editing-form').text("test");
+    //});
+
+    $(function() {
+
+        var $newItemButton = $('#edit-1');
+        var $newItemForm = $('#newItemForm');
+        var $textInput = $('input:text');
+
+        $newItemButton.show();
+        $newItemForm.hide();
+
+        $('#edit-1').on('click', function(){
+            $newItemButton.hide();
+            $newItemForm.show();
+        });
+
+
+        $newItemForm.submit(function(e){
+            e.preventDefault();
+            var newText = $textInput.val();
+            $('li:last').after('<li>' + newText + '</li>');
+            $newItemForm.hide();
+            $newItemButton.show();
+            $textInput.val('');
+        });
+
+    });
+});
