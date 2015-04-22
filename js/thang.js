108ccecabff6b4e0a2ffafffa53a69ff46d51240
ThangTheMan <thang@fromttm.com>
2015-04-22 14:18:14 -0700

    Thang modal update

--- js/thang.js
+++ js/thang.js
@@ -2,49 +2,24 @@
  * Created by ThangTheMan on 4/21/15.
  */
 
-
 $(document).ready(function() {
     //Button to pop-out form and replace variable.
     //$('#edit-1').click(function () {
     //    $('#editing-form').text("test");
     //});
 
-    $(function() {
-//What happen when click
-        var $newItemButton = $('#edit-1');
-        var $newItemForm = $('#newItemForm');
-        var $textInput = $('input:text');
-        var nam
-        $newItemButton.show();
-        $newItemForm.hide();
+var name;
+    //Click the button:
+    $('#formSubmit').submit(function(){
+            name = $('#inputName').text();
+
+        }
+
+
+    )
+console.log(name+"   test");
 
 
 
-
-//what inside the form
-
-        $('#edit-1').on('click', function(){
-            $newItemButton.hide();
-            $newItemForm.show();
-        });
-
-
-
-
-
-
-
-
-        //Click Submit
-        $newItemForm.submit(function(e){
-            e.preventDefault();
-            var newText = $textInput.val();
-            $('li:last').after('<li>' + newText + '</li>');
-            $newItemForm.hide();
-            $newItemButton.show();
-            $textInput.val('');
-        });
-
-    });
 });
 
